d50260c43ca440380ecd42c51da44b35a5cacadd
pwh0607Git <pwhof0607@gmail.com>
2025-03-31 17:58:52 +0900

    v0.0.16: feat: Player silouette 추가

---
+++ Assets/_Scripts/Dungeon/Interactive/Door.cs
@@ -0,0 +1,23 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] Collider blockDoor;
+    [SerializeField] Transform leftDoor;
+    [SerializeField] Transform rightDoor;
+    public bool isLocked{get; private set;} = true;
+
+    void Start()
+    {
+        StageLogicManager.I.OnOpenLogicCompleted += OpenDoor;
+    }
+
+    void OpenDoor(){
+        isLocked = false;
+        if(blockDoor.gameObject.activeSelf) Destroy(blockDoor.gameObject);
+
+        leftDoor.DOLocalRotate(new Vector3(0, 100f, 0), 1, RotateMode.FastBeyond360);
+        rightDoor.DOLocalRotate(new Vector3(0, -100f, 0), 1, RotateMode.FastBeyond360);
+    }
+}
---
+++ Assets/_Scripts/Dungeon/Manager/StageLogicManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StageLogicManager : BehaviourSingleton<StageLogicManager>
+{
+    protected override bool IsDontDestroy() => false;
+    
+    public List<DoorLogic> logics = new ();
+    public UnityAction OnOpenLogicCompleted;
+    [SerializeField] List<Door> doors = new();
+
+    void Start()
+    {
+        doors.Clear();
+        doors = new List<Door>(FindObjectsByType<Door>(FindObjectsSortMode.None));
+    }
+
+    //test
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            OnOpenLogicCompleted?.Invoke();
+        }
+    }
+
+    public void UseKey(AnswerKey answerKey){
+        // logics.
+    }
+}
+
+[Serializable]
+public class DoorLogic{
+    public Door door;
+    public AnswerKey key;
+}
